1f8fe83595bf1a7b23e815b29b4b19137ba96b94
Jack Gutteridge <jack@kingbrick.co.uk>
2024-01-19 20:42:13 +0000

    404 page

---
+++ app/src/routes/404.html/+page.svelte
@@ -0,0 +1,8 @@
+<div class="container error-container">
+  <h1 class="main-heading">Page not found</h1>
+  <p>The page you have navigated to could not be found.</p>
+  <a class="main-next-link" href="/">Go back to homepage</a>
+</div>
+<svelte:head>
+  <title>Page not found</title>
+</svelte:head>

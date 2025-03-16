6bcfcf69a13b6a8820434c81669099fa1f4df144
Jack Gutteridge <jack@kingbrick.co.uk>
2025-03-16 22:18:40 +0000

    Forgot the links page

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -2,20 +2,20 @@
   let { data } = $props();
   let { title, detail, links, projectLinks } = $derived(data);
 </script>
-<div class="container links-container">
+<div class="max-w-[65ch] mx-auto">
   <h1 class="text-3xl pt-6 text-center max-w-[35ch] mx-auto text-balance">{ title }</h1>
   <span class="text-slate-500 text-sm pt-2 pb-6 text-center block">{ detail }</span>
   <dl>
   {#each links as {href, title, description}}
-    <dt><a {href}>{title}</a></dt>
-    <dd>{description}</dd>
+    <dt><a {href} class="text-slate-600 underline">{title}</a></dt>
+    <dd class="mb-4">{description}</dd>
   {/each}
   </dl>
-  <h2>Projects</h2>
+  <h2 class="text-xl text-slate-800 mt-10 mb-4">Projects</h2>
   <dl>
   {#each projectLinks as {href, title, description}}
-    <dt><a {href}>{title}</a></dt>
-    <dd>{description}</dd>
+    <dt><a {href} class="text-slate-600 underline">{title}</a></dt>
+    <dd class="mb-4">{description}</dd>
   {/each}
   </dl>
   <a class="text-slate-500 my-6 underline block" href="/">Go to homepage</a>

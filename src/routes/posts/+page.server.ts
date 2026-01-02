010161d229614eb7681756e36fc72032813460dc
jngutteridge <jack@kingbrick.co.uk>
2026-01-02 17:01:41 +0000

    Quick fix: Post years were reversed

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -3,14 +3,18 @@ import { fetchPosts } from '$lib/utils';
 export async function load() {
   const posts = await fetchPosts();
 
-  const postsByYear = posts.reduce((acc, post) => {
+  const postsByYear = Object.values(posts.reduce((acc, post) => {
     const year = new Date(post.date).getFullYear().toString();
     if (!acc[year]) {
       acc[year] = { year, posts: [] };
     }
     acc[year].posts.push(post);
     return acc;
-  }, {} as Record<string, {year: string, posts: typeof posts}>);
+  }, {} as Record<string, {year: string, posts: typeof posts}>))
+    .sort((a, b) => {
+		  return parseInt(b.year) - parseInt(a.year);
+	  }
+  );
 
-  return { postsByYear: Object.values(postsByYear), count: posts.length }
+  return { postsByYear, count: posts.length }
 }

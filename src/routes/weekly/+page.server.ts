688c0219c7b92c1068775691f8a633c79eef244a
jngutteridge <jack@kingbrick.co.uk>
2026-01-12 03:56:31 +0000

    Fix weekly retro by year ordering

--- src/routes/weekly/+page.server.ts
+++ src/routes/weekly/+page.server.ts
@@ -3,14 +3,19 @@ import { fetchWeeklyPosts } from '$lib/utils';
 export async function load() {
   const posts = await fetchWeeklyPosts();
 
-  const postsByYear = posts.reduce((acc, post) => {
+  const postsByYear = Object.values(posts.reduce((acc, post) => {
     const year = new Date(post.date).getFullYear().toString();
     if (!acc[year]) {
       acc[year] = { year, posts: [] };
     }
     acc[year].posts.push(post);
     return acc;
-  }, {} as Record<string, {year: string, posts: typeof posts}>);
+  }, {} as Record<string, {year: string, posts: typeof posts}>))
+    .sort((a, b) => {
+		  return parseInt(b.year) - parseInt(a.year);
+	  }
+  );
 
-  return { postsByYear: Object.values(postsByYear), count: posts.length }
+
+  return { postsByYear, count: posts.length }
 }
